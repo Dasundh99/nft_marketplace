51d8598cf2711a28871fd1c52863c68800fb8d44
Dasundh99 <dasundhammasara9@gmail.com>
2025-07-31 19:44:07 +0530

    home main sections

--- client/src/pages/Categories.tsx
+++ client/src/pages/Categories.tsx
@@ -4,12 +4,12 @@ const categories = ["Gold", "Petroleum", "Gem", "Jewellery", "Silver", "Platinum
 
 const Categories: React.FC = () => {
   return (
-    <section className="py-6 px-6 bg-gray-950">
+    <section className="py-4 px-4 bg-black">
       <div className="flex flex-wrap justify-left gap-2">
         {categories.map((cat) => (
           <button
             key={cat}
-            className="px-4 py-2 border border-gray-200 text-gray-200 hover:bg-gray-500 hover:text-black rounded-lg transition"
+            className="px-4 py-2 border border-white text-white hover:bg-blue-500 hover:text-black rounded-md transition"
           >
             {cat}
           </button>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -10,22 +10,77 @@ const Home: React.FC = () => {
           <Categories />
         </div>
 
-        {/* Five horizontal sections */}
         <div className="flex flex-col">
-          <div className="h-[500px] bg-gray-300 flex items-center justify-center">
+          <div className="h-[500px] bg-black flex items-center justify-center">
             <p>Section 1</p>
           </div>
-          <div className="h-[500px] bg-gray-400 flex items-center justify-center">
-            <p>Section 2</p>
+
+
+          <div className="h-[400px] bg-black flex-col flex items-center justify-center gap-5 p-5">
+            <div>
+              <div>Trending Assets</div>
+            </div>
+            
+            <div className="w-full flex-1 flex gap-5">
+              <div className="w-1/3 flex justify-center items-center border border-gray-500 rounded-md bg-gray-200 text-black">1</div>
+              <div className="w-1/3 flex justify-center items-center border border-gray-500 rounded-md bg-gray-200 text-black">2</div>
+              <div className="w-1/3 flex justify-center items-center border border-gray-500 rounded-md bg-gray-200 text-black">3</div>
+            </div>
+
+            <div className="w-full flex-1 flex gap-5">
+              <div className="w-1/3 flex justify-center items-center border border-gray-500 rounded-md bg-gray-200 text-black">4</div>
+              <div className="w-1/3 flex justify-center items-center border border-gray-500 rounded-md bg-gray-200 text-black">5</div>
+              <div className="w-1/3 flex justify-center items-center border border-gray-500 rounded-md bg-gray-200 text-black">6</div>
+            </div>
           </div>
-          <div className="h-[500px] bg-gray-500 flex items-center justify-center">
-            <p>Section 3</p>
+
+
+          <div className="h-[300px] bg-black flex-col flex items-center justify-center gap-5 p-5">
+            <div>
+              <div>Weekly Highest Sales </div>
+            </div> 
+            
+            <div className="w-full flex-1 flex gap-5">
+              <div className="w-1/3 flex justify-center items-center border border-gray-500 rounded-md bg-gray-200 text-black">1</div>
+              <div className="w-2/3 flex justify-center items-center border border-gray-500 rounded-md bg-gray-200 text-black">2</div>
+            </div>
           </div>
-          <div className="h-[500px] bg-gray-700 flex items-center justify-center">
-            <p>Section 4</p>
+  
+
+          <div className="h-[500px] bg-black flex-col flex items-center justify-center gap-5 p-5">
+            <div>
+              <div>Highest Assets</div>
+            </div>
+            
+            <div className="w-full flex-1 flex gap-5">
+              <div className="w-1/3 flex justify-center items-center border border-gray-500 rounded-md bg-gray-200 text-black">1</div>
+              <div className="w-1/3 flex justify-center items-center border border-gray-500 rounded-md bg-gray-200 text-black">2</div>
+              <div className="w-1/3 flex justify-center items-center border border-gray-500 rounded-md bg-gray-200 text-black">3</div>
+            </div>
+
+            <div className="w-full flex-1 flex gap-5">
+              <div className="w-1/3 flex justify-center items-center border border-gray-500 rounded-md bg-gray-200 text-black">4</div>
+              <div className="w-1/3 flex justify-center items-center border border-gray-500 rounded-md bg-gray-200 text-black">5</div>
+              <div className="w-1/3 flex justify-center items-center border border-gray-500 rounded-md bg-gray-200 text-black">6</div>
+            </div>
           </div>
-          <div className="h-[500px] bg-gray-900 flex items-center justify-center">
-            <p>Section 5</p>
+
+
+          <div className="h-[300px] bg-black flex-col flex items-center justify-center gap-5 p-5">
+            <div>
+              <div>How NFT Works</div>
+            </div> 
+
+            <div>
+              <div>"A fusion of timeless elegance and digital innovation — this NFT captures</div>
+            </div> 
+            
+            <div className="w-full flex-1 flex gap-5">
+              <div className="w-1/4 flex justify-center items-center border border-gray-500 rounded-md bg-gray-200 text-black">1</div>
+              <div className="w-1/4 flex justify-center items-center border border-gray-500 rounded-md bg-gray-200 text-black">2</div>
+              <div className="w-1/4 flex justify-center items-center border border-gray-500 rounded-md bg-gray-200 text-black">3</div>
+              <div className="w-1/4 flex justify-center items-center border border-gray-500 rounded-md bg-gray-200 text-black">4</div>
+            </div>
           </div>
         </div>
       </div>
