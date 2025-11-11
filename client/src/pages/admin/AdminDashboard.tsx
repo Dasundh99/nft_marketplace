14d993da13ee791c2399709a57c3f01178434080
Kavi-Dew-23 <dewapuraka@gmail.com>
2025-11-11 17:42:24 +0530

    Nft dashboard

--- client/src/pages/admin/AdminDashboard.tsx
+++ client/src/pages/admin/AdminDashboard.tsx
@@ -1,114 +1,50 @@
-import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
-import React, { useEffect, useState } from "react";
-import { db } from "../../utils/firebase";
+import { useState } from "react"
+import ListingTable from "../../components/Admin/ListingsTable"
+import NftTable from "../../components/Admin/NftTable"
 
-interface NFT {
-    id: string;
-    name: string;
-    image: string;
-    owner: string;
-    mintAddress: string;
-    createdAt: string;
-    manufactureDate: string;
-    expiryDate: string;
-    certificateUrl: string;
-    description: string;
-    metadataUri: string;
-}
+const AdminDashboard: React. FC = () => {
+    const [selectedTab, setSelectedTab] = useState("nfts");
 
-const AdminDashboard: React.FC = () => {
-    const [nfts, setNfts] = useState<NFT[]>([]);
+    const tabs = [
+        {key: "nfts", label: "NFTs"},
+        {key: "listings", label: "Listings"}
+    ];
 
-    useEffect(() => {
-        //create a firestore query
-        const q = query(collection(db, "nfts"), orderBy("createdAt","desc"));
-
-        // set a real time listener on the nfts collection
-        const unsubscribe = onSnapshot(q, (snapshot) => {
-            const data = snapshot.docs.map((doc) => ({
-                id:doc.id,
-                ...doc.data(),
-            })) as NFT[];
-            
-            //update the react state with the fetched nft data
-            setNfts(data);
-        });
-
-        // cleanup the listener when components unmount
-        return () => unsubscribe();
-    }, []);
-
-    // handles the both created date types Timestamp and string
-    const formattedDate = (createdAt: any) => {
-        if(!createdAt) return "-";
-        try{
-            if(createdAt.toDate){
-                return createdAt.toDate().toLocaleDateString(); //Timestamp
-            }
-            return new Date(createdAt).toLocaleDateString(); //string
+    const renderContent = () => {
+        switch (selectedTab){
+            case "nfts":
+                return <NftTable/>;
+             case "listings":
+                return <ListingTable/>;
         }
-        catch {
-            return "Imvalid Date";
-        }
-    };
-
+    }
 
     return (
-        <div className="min-h-screen bg-gray-900 text-white p-8">
+        <div className="min-h-screen bg-gray-900 text-white p-6">
             <h1 className="text-3xl font-bold text-white mb-8 text-center">
-                NFT Marketplace Dashoard
+                Admin Dashboard
             </h1>
-            {/**Mfts transaction table */}
-            <div className="overflow-x-auto bg-gray-800 rounded-2xl shadow-lg border border-gray-700">
-                <table className="min-w-full text-sm">
-                    <thead className="bg-gray-700">
-                        <tr>
-                            <th className="p-3">Image</th>
-                            <th className="p-3">Created Date</th>
-                            <th className="p-3">Name</th>
-                            <th className="p-3">Owner</th>
-                            <th className="p-3">Manufactured Date</th>
-                            <th className="p-3">Expiry Date</th>
-                            <th className="p-3">Mint Address</th>
-                            <th className="p-3">Certificate</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {nfts.length === 0 && (
-                            <tr>
-                                <td colSpan={8} className="text-center py-10 text-gray-300 italic">
-                                    No Nfts minted yet
-                                </td>
-                            </tr>
-                        )}
-                        {nfts.map((nft) => (
-                            <tr
-                            key={nft.id}
-                            className="border-t border-gray-700 hover:bg-gray-700">
-                                <td className="p-3">
-                                    <img src={nft.image} alt={nft.name} 
-                                    className="w-14 h-14 rounded-lg object-cover border border-gray-800"/>
-                                </td>
-                                <td className="p-3">{formattedDate(nft.createdAt)}</td>
-                                <td className="p-3 font-medium">{nft.name}</td>
-                                <td className="p-3 text-gray-400">{nft.owner}</td>
-                                <td>{nft.manufactureDate}</td>
-                                <td>{nft.expiryDate}</td>
-                                <td>{nft.mintAddress}</td>
-                                <td className="p-3">
-                                    <a href="{nft.certificateUrl}"
-                                    target="_blank"
-                                    className="text-blue-500 underline hover:text-blue-400">
-                                        View
-                                    </a>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+
+            {/**tabs selection */}
+            <div className="flex justify-left ml-10 mb-8 space-x-3 border-b border-gray-900 pb-2">
+                {tabs.map((tab) => (
+                    <button key={tab.key}
+                    onClick={() => setSelectedTab(tab.key)}
+                    className={`px-40 py-2 rounded-t-lg font-medium transition-colors ${
+                        selectedTab === tab.key
+                          ? "bg-indigo-600 text-white"
+                          : "bg-gray-800 text-gray-400 hover:bg-gray-700 hover:text-white"
+                      }`}>
+                        {tab.label}
+                    </button>
+                ))}
+            
+            </div>
+            {/**content selection */}
+            <div className="mt-4">
+                {renderContent()}
             </div>
         </div>
-    );
-};
-
+    )
+}
 export default AdminDashboard;
